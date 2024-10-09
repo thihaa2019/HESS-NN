86d3ce851744b4df0df44590f0e87f57f2394f09
John Lain <john.henry.lain@gmail.com>
2024-10-09 12:28:20 -0700

    Updated wind_process to gerenate multiple simulations

--- wind_process.py
+++ wind_process.py
@@ -1,31 +1,33 @@
 import numpy as np
-def wind(kv, sig_v, sig_vpi, T, n = None, BM = None):
+def wind(kv, sig_v, sig_vpi, T, n = None, nsims = None, BMwind = None, BMprice = None):
     '''
     Numerical approximation of a realization of a solution to wind stochastic differential equation.
     
     Inputs
-        kv: parameter
+        kv: parameter of SDE
         sig_v: Standard deviation of wind
         sig_vpi: Covaraince of wind and price
-        T: Time
-        n: numer of steps
-        BM: Pre-generated standard brownian motions
+        T: final time
+        n: number of time steps
+        BMwind: Pre-generated standard brownian motions, matrix with shape (nsims, n)
 
-    Output: Array conatining values of wind speed over time
+    Output: Matrix conatining values of wind speed over time with shape (nsims, n + 1)
     '''
-    if type(BM) == type(None): #Checks to see if function is using pregenerated Brownian motion
+    if type(BMwind) == type(None): #Checks to see if function is using pregenerated Brownian motion
         n = int(n)
-        cov = np.reshape(np.array([1, 0, 0, 1]), (2,2))
-        BM = np.random.multivariate_normal([0,0], cov, size=n) #generates brownian motions
+        h = T/n
+        BMwind = np.random.normal(scale = np.sqrt(h), size = nsims*n).reshape(nsims, n)
+        BMprice = np.random.normal(scale = np.sqrt(h), size = nsims*n).reshape(nsims, n) #generates brownian motions
     else:
-        n = np.size(BM[:,0])
-    h = T/n
+        n = np.size(BMwind[0,:])
+        h = T/n
     sigmav = lambda s: 8*(1 + 0.375*np.sin(np.pi/(12*h)*(s + 2/h))) #functions used in SDE
     dsigmav = lambda s: 8*0.375*np.cos(np.pi/(12*h)*(s + 2/h))*(np.pi/(12*h))
-    s = 0
-    v = np.zeros(n + 1)
-    v[0] = np.random.normal(sigmav(0), 0.226)
-    for i in range(0 + 1, n + 1): #Numerically solves SDE
-        v[i] = v[i - 1] + kv*(sigmav(s) + 1/kv*dsigmav(s) - v[i - 1])*h + np.sqrt(h)*sig_v*v[i - 1]*BM[i - 1, 0] + np.sqrt(h)*sig_vpi*v[i - 1]*BM[i - 1, 1]
+    s = 0 #initial time
+    v = np.zeros(shape = (nsims, (n + 1))) 
+    v[:,0] = np.random.normal(sigmav(0), 0.226, nsims) #initial wind speed
+    for i in range(0, n): #Numerically solves SDE for each simulation
+        v[:,i + 1] = v[:,i] + kv*(sigmav(s) + 1/kv*dsigmav(s) - v[:,i])*h + \
+            sig_v*v[:,i]*BMwind[:,i] + sig_vpi*v[:,i]*BMprice[:,i]
         s = s + h
     return v
