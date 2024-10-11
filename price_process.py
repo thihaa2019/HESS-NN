679c65c68be81de7e3f938773708ffe7b3e94608
jcbrandonsu <jiachengsu@ucsb.edu>
2024-10-10 19:15:33 -0700

    updated .py file

--- price_process.py
+++ price_process.py
@@ -12,39 +12,45 @@ alpha_v = 0.375
 gamma = np.pi / (12 * h)
 psi_v = 2 / h
 
-def price_sde(T, h, k_pi, sigma_pi_v, sigma_pi_pi):
+def price_sde(T, N, k_pi, sigma_pi_v, sigma_pi_pi, simulation_times=None, wind_BM = None, price_BM = None):
 
     '''
     Simulation of the price process
 
     Inputs: 
     T: total time
-    h: time step size
+    N: number of steps
     k_pi: mean reversion rate for price
     sig_pi_v: correlation of electricity price and wind speed
     sig_pi_pi: volatility of the price process
-    wind_BM: standard brownian motion for the wind power
-    price_BM: standard brownian motion for the spot price
+    simulation_times: how many times to simulate the process, default None
+    wind_BM: standard brownian motion for the wind power, default None
+    price_BM: standard brownian motion for the spot price, default None
 
     Output:
-    A matrix contains the price of electricity at each time point
+    A matrix containing the price of electricity at each time point
     '''
-
-    N = int(T/h)
-    cov = np.reshape(np.array([1, 0, 0, 1]), (2,2))
-    brownianmotion = np.random.multivariate_normal([0,0], cov, size=N)
-
+    h = T / N
+    
+    if price_BM is None or wind_BM is None:
+        if price_BM is None: 
+            price_BM = np.random.normal(0, np.sqrt(h), size = (simulation_times, N))
+        
+        if wind_BM is None:
+            wind_BM = np.random.normal(0, np.sqrt(h), size = (simulation_times, N))
+    else:
+        h = T / np.size(price_BM[0, :])
+    
     theta_v = lambda s: theta_v_bar * (1 + alpha_v * np.sin(gamma*(s + psi_v)))
     dtheta_v = lambda s: theta_v_bar * alpha_v * np.cos(gamma * (s + psi_v)) * gamma
 
     s = 0 
-    price = np.zeros(N + 1)
-    price[0] = np.random.normal(theta_v(0), 0.075)
+    price = np.zeros((simulation_times, N + 1))
+    price[:, 0] = np.random.normal(theta_v(0), 0.075, simulation_times)
     
-    for i in range(1, N + 1):
-        price[i] = price[i - 1] + k_pi * (theta_v(s) + 1/k_pi * dtheta_v(s) - price[i - 1]) * h \
-            + np.sqrt(h) * sigma_pi_v * price[i - 1] * brownianmotion[i-1, 0] \
-            + np.sqrt(h) * sigma_pi_pi * price[i-1] * brownianmotion[i-1, 1]
+    for i in range(0, N):
+        price[:, i+1] = price[:, i] + k_pi * (theta_v(s) + 1/k_pi * dtheta_v(s) - price[:, i]) * h \
+            + np.sqrt(h) * sigma_pi_v * price[:, i] * wind_BM[:, i] \
+            + np.sqrt(h) * sigma_pi_pi * price[:, i] * price_BM[:, i]
         s += h
     return price
-
